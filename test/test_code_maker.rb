05b2fbeac3f377e2c3631145329d6c6fe452dd9d
Oscar <oskarromero3@gmail.com>
2015-09-27 03:32:45 +0100

    Add test for exact_match and partial_match. Refactor printer to output arrays

--- test/test_code_maker.rb
+++ test/test_code_maker.rb
@@ -9,9 +9,9 @@ require "stringio"
 class CodemakerTest < Minitest::Test
   def setup
     difficulty  = :beginner
-    printer     = Mastermind::Oscar::Printer.new
+    @printer     = Mastermind::Oscar::Printer.new
     recorder    = Mastermind::Oscar::RecordManager.new(difficulty,StringIO.new("Jeff\n"))
-    @client     = Mastermind::Oscar::Codemaker.new(difficulty,printer,recorder)
+    @client     = Mastermind::Oscar::Codemaker.new(difficulty,@printer,recorder)
   end
 
   def test_generate_code
@@ -34,15 +34,65 @@ class CodemakerTest < Minitest::Test
 
   def test_valid_input
     input = %w{rrrr rgbb}
-    input.each do |i|
-      assert @client.verify_input i
-    end
+    input.each {|i| assert @client.is_valid_input?(i)}
   end
 
   def test_invalid_input
     input = %w{t k l o j e} << "a"*5 
-    input.each do |i|
-      assert !@client.verify_input(i)
+    input.each {|i| assert !@client.is_valid_input?(i)}
+  end
+
+  def test_valid_quit
+    input = %w{q quit quiet quays queen}
+    input.each {|i| assert @client.quit?(i)}
+  end
+
+  def test_invalid_quit
+    input = ('a'..'z').to_a
+    input.delete('q')
+    input.each {|i| assert !@client.quit?(i)}
+  end
+
+  def test_valid_cheat
+    input = %w{c cheat chair change call come}
+    input.each {|i| assert @client.cheat?(i)}
+
+  end
+
+  def test_invalid_cheat
+    input = ('a'..'z').to_a
+    input.delete('c')
+    input.each {|i| assert !@client.cheat?(i)}
+  end
+
+  def test_exact_match
+    code = [] ;input = []; expect= []
+
+    code  << %w{r r g g} << %w{r r g g} << %w{r r g g}  << %w{g r r b} << %w{r b g y} << %w{y y y y}  << %w{b b g r}
+
+    input << %w{r r r r}  << %w{g r r g} << %w{y y g g} << %w{b g g r} << %w{i j k l} << %w{y y v y} << %w{b b g r}
+
+    expect << [0,1]  << [1,3] << [2,3] << [] << [] << [0,1,3] << [0,1,2,3]
+
+    code.each_with_index do |arr, index|
+      assert_equal(expect[index], @client.exact_match(arr, input[index]))
+    end
+
+  end
+
+  def test_partial_match
+    code = []; input = []; exact = []; partial = []
+
+    code  << %w{r r g g} << %w{r r g g} << %w{r r g g}  << %w{g r r b} << %w{r b g y} << %w{y y y y}  << %w{b b g r}
+
+    input << %w{r r r r}  << %w{g r r g} << %w{y y g g} << %w{b g g r} << %w{i j k l} << %w{y y v y} << %w{b b g r}
+
+    exact << [0,1]  << [1,3] << [2,3] << [] << [] << [0,1,3] << [0,1,2,3]
+
+    partial << 0 << 2 << 0 << 3 << 0 << 0 << 0
+
+    code.each_with_index do |arr, index|
+      assert_equal(partial[index], @client.partial_match(arr, input[index], exact[index]))
     end
   end
 
