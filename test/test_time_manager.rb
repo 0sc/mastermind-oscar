326d8c927a1dcb171e12a56c621d2094f57199fb
Oscar <oskarromero3@gmail.com>
2015-09-27 15:09:09 +0100

    Add test for getting and evaluation gameplay time

--- test/test_time_manager.rb
+++ test/test_time_manager.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require "mastermind/oscar/time_manager"
+require "time"
 
 class TimeManagerTest < Minitest::Test
   def setup
@@ -19,11 +20,24 @@ class TimeManagerTest < Minitest::Test
 
   def test_evaluate
     x = 18290, 290 
-    y =  3600, 60
+    y = 3600, 60
     ans = [5, 290], [4, 50]
 
     x.each_index do |i|
       assert_equal(ans[i], @client.evaluate(x[i], y[i]))
     end
   end
+
+  def test_to_string
+    input = [[1, 'hour'],[5, 'minute'],[0, 'second'],[1, 'second'],[6, 'hour']]
+    expect = ['1 hour ', '5 minutes ', nil, '1 second ', '6 hours ']
+
+    input.each_index do |i|
+      time = input[i][0]
+      unit = input[i][1]
+      assert_equal(expect[i], @client.to_string(time, unit))
+    end
+  end
+
+
 end
