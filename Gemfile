875033a9544457b9148a91915f0d1963b3c0b7fb
vagrant <vagrant@precise32.(none)>
2015-10-08 10:22:06 +0000

    Add coverage badge

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in mastermind-oscar.gemspec
 gemspec
+gem 'simplecov-shield', github: 'aterris/simplecov-shield'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require 'simplecov'
 SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/reporters'
+require "simplecov-shield"
 
 require "mastermind/oscar/game_manager"
 require "mastermind/oscar/record_manager"
@@ -11,3 +12,5 @@ require "mastermind/oscar/codemaker"
 require "mastermind/oscar/time_manager"
 require "mastermind/oscar/printer"
 require "stringio"
+
+SimpleCov.formatter = SimpleCov::Formatter::ShieldFormatter
